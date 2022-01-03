716b7adffbb496911489ab3e31cfb56d95b13bfa
aqfa11 <80084765+aqfa11@users.noreply.github.com>
2022-01-03 10:45:33 +0700

    Add project files.

---
+++ ServerConfigRest_090_Aqfa_Allowdya_Diamytara/Program.cs
@@ -0,0 +1,32 @@
+﻿using ServiceRest_090_Aqfa_Allowdya_Diamytara;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerConfigRest_090_Aqfa_Allowdya_Diamytara
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostObj = null;
+            try
+            {
+                hostObj = new ServiceHost(typeof(TI_UMY));
+                hostObj.Open();
+                Console.WriteLine("Server is Ready!!!");
+                Console.ReadLine();
+                hostObj.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObj = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
